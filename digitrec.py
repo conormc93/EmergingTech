1f3c6095acc00c5bbe7ccdfb59df65f5b74da73f
conormc93 <conordmcgrath93@gmail.com>
2018-11-28 22:55:09 +0000

    loading and reshaping data - converting values to 0 or 1

--- digitrec.py
+++ digitrec.py
@@ -1,14 +1,23 @@
-from flask import Flask
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+import numpy as np
 
-app = Flask(__name__)
+batch_size = 128
+num_classes = 10
+epochs = 2
 
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-@app.route('/')
-def hello_world():
-    return app.send_static_file('index.html')
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 
-
-# Main method.
-if __name__ == "__main__":
-    # Run the app.
-    app.run()
